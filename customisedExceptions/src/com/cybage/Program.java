dc2e422726a24b9e02400354a58a620aa9e2f642
Pawan Kasar <97679680+pawankas@users.noreply.github.com>
2023-02-20 01:58:01 +0530

    ce

--- customisedExceptions/src/com/cybage/Program.java
+++ customisedExceptions/src/com/cybage/Program.java
@@ -6,8 +6,31 @@ package com.cybage;
  * :TooOldException
  * :InSufficientFundsException, etc */
 
+ class tooYoungException extends RuntimeException{
+	tooYoungException(String s){
+		super(s);
+	}
+}
+
+class tooOldException extends RuntimeException{
+	tooOldException(String s){
+		super(s);
+	}
+}
+
+
 public class Program{
 	public static void main(String[] args) {
-		
+		int age = 85;
+//		int age = 10;
+		if (age > 60) {
+			throw new tooOldException("Your age is too much.");
+		}
+		else if (age < 18) {
+			throw new tooYoungException("You are too young to get married.");
+		}
+		else {
+			System.out.println("You will get match details soon by email.");
+		}
 	}
 }
