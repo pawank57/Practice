86b899570f6031fc8420d670e4a602593fd33a42
pawan <pawankas@cybage.com>
2023-02-20 16:21:18 +0530

    change

--- Top10Exceptions/src/com/cybage/Program.java
+++ Top10Exceptions/src/com/cybage/Program.java
@@ -14,7 +14,14 @@ package com.cybage;
  * */
 
 public class Program{
+	public static void m1() {
+		m2();
+	}
+	public static void m2() {
+		m1();
+	}
 	public static void main(String[] args) {	
+		m2();									//StackOverflowError
 	}
 	
 	public static void main4(String[] args) {
