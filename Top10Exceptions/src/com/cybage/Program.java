5f282c88716720db5d458686a0a5d257bc568928
pawan <pawankas@cybage.com>
2023-02-20 17:47:49 +0530

    hfgf

--- Top10Exceptions/src/com/cybage/Program.java
+++ Top10Exceptions/src/com/cybage/Program.java
@@ -14,13 +14,17 @@ package com.cybage;
  * */
 
 public class Program{
+	public static void main(String[] args) {
+//		static int x = 10/0;					//CE: Illegal modifier
+	}
+	
 	public static void m1() {
 		m2();
 	}
 	public static void m2() {
 		m1();
 	}
-	public static void main(String[] args) {	
+	public static void main5(String[] args) {	
 		m2();									//StackOverflowError
 	}
 	
