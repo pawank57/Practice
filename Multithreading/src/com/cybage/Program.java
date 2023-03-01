7899ea265a160200ea92b9e800a8637572284afe
pawan <pawankas@cybage.com>
2023-03-01 18:05:56 +0530

    changes

--- Multithreading/src/com/cybage/Program.java
+++ Multithreading/src/com/cybage/Program.java
@@ -26,19 +26,20 @@ class A extends Thread{
 }
 
 public class Program {
-	public static void main(String[] args) {
+	public static void main1(String[] args) {
 		myThread mt = new myThread();
-//		mt.start();						//In the case of t.start a new thread will be created which is responsible for the execution of run method.
+		mt.start();						//In the case of t.start a new thread will be created which is responsible for the execution of run method.
 		mt.run();						//But in the case of t.run, a new thread wont be created, and the run method will be executed just like a nomal method call by the main thread.
 		for ( int j=0; j <= 10; j++) {	
 			System.out.println("Main Thread.");
 		}
 	}
 	
-	public static void main1(String[] args) throws InterruptedException {
+	public static void main(String[] args) throws InterruptedException {
 		A a = new A();
 		a.start();
 		a.sleep(1000);
+//		a.start();						//java.lang.IllegalThreadStateException
 		a.run();
 	}
 }
