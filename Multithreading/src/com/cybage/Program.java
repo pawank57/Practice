8cbbeca5c2d710b90d789516ba85990e8bb50314
pawan <pawankas@cybage.com>
2023-03-01 17:48:14 +0530

    changes

--- Multithreading/src/com/cybage/Program.java
+++ Multithreading/src/com/cybage/Program.java
@@ -8,6 +8,14 @@ package com.cybage;
  * 
  * */
 
+class myThread extends Thread{
+	public void run() {
+		for (int i = 0; i <= 5; i++) {
+			System.out.println("Child Thread.");
+		}
+	}
+}
+
 class A extends Thread{
 	public void run() {
 		for( int i = 0; i <= 5; i++) {
@@ -18,7 +26,16 @@ class A extends Thread{
 }
 
 public class Program {
-	public static void main(String[] args) throws InterruptedException {
+	public static void main(String[] args) {
+		myThread mt = new myThread();
+//		mt.start();						//In the case of t.start a new thread will be created which is responsible for the execution of run method.
+		mt.run();						//But in the case of t.run, a new thread wont be created, and the run method will be executed just like a nomal method call by the main thread.
+		for ( int j=0; j <= 10; j++) {	
+			System.out.println("Main Thread.");
+		}
+	}
+	
+	public static void main1(String[] args) throws InterruptedException {
 		A a = new A();
 		a.start();
 		a.sleep(1000);
