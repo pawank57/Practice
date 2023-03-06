ae2baddb5a4da8b9a705baa3c412f7081c154b66
pawan <pawankas@cybage.com>
2023-03-06 18:14:05 +0530

    change

--- Multithreading4/src/com/cybage/Program.java
+++ Multithreading4/src/com/cybage/Program.java
@@ -7,5 +7,8 @@ class myThread extends Thread{
 public class Program {
 	public static void main(String[] args) {
 		System.out.println(Thread.currentThread().getName());
+		myThread t = new myThread();
+		String thread = t.getName();
+		System.out.println(thread);
 	}
 }
